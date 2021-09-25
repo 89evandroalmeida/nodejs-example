aa6ecdfc772532596e896fa1fe645fc36920bddb
Evandro Almeida <89evandroalmeida@gmail.com>
2021-09-25 16:19:58 -0300

    Materialize starter theme

--- src/index.js
+++ src/index.js
@@ -2,8 +2,21 @@ const app = require("./app");
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("GET / OK");
+  res.render('pages/index');
 })
+
+app.get("/page-1", (req, res) => {
+  res.render('pages/page1');
+})
+
+app.get("/page-2", (req, res) => {
+  res.render('pages/page2');
+})
+
+app.get("/page-3", (req, res) => {
+  res.render('pages/page3');
+})
+
 
 app.listen(port, function () {
   console.log(`Listening on port ${port}`);
